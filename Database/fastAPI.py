4b8c2fb3d748a665c412833f07c0ff504f27ff23
K_Vebere <vebere2204@gmail.com>
2025-02-27 11:02:46 +0000

    users.py and backend

--- Database/fastAPI.py
+++ Database/fastAPI.py
@@ -1,7 +1,12 @@
 from typing import Union
 from fastapi import FastAPI
+from lib.backend import create_db_and_tables
 
 app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
 
 @app.get("/")
 def read_root():
@@ -9,4 +14,4 @@ def read_root():
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q} 
+    return {"item_id": item_id, "q": q}  
