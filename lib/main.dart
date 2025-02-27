36eea4bab67092cc7563f616f1217ad408d7aa4f
Jodie <jodiewlfan@gmail.com>
2025-02-27 14:48:48 +0000

    completed base mood entry page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart'; // FOR DATE AND TIME
 
 void main() {
   runApp(const MainApp());
@@ -40,7 +41,8 @@ class _HomePageState extends State<HomePage> {
     Widget page;
 
     switch (selectedIndex) {
-      // figure out how to make the title page a title page. it shouldn't be accessible through the navigation menu just there for now.
+      // figure out how to make the title page a title page. it shouldn't be accessible through the
+      // navigation menu just there for now.
       case 0:
         page = TitlePage();
         break;
@@ -131,14 +133,34 @@ class MoodLoggingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // return Text('You are on the mood logging page!');
+    return MoodEntry(
+      entry:
+          "Today I am feeling very Lorem ipsum odor amet, consectetuer adipiscing elit. Quam ullamcorper lacinia vehicula ornare lacinia interdum tincidunt cras. Est cras facilisis mauris mattis nascetur. Ligula ipsum bibendum himenaeos sed tortor nec. Cras dapibus ridiculus a nibh ridiculus interdum condimentum cursus. Interdum odio sapien vitae, mattis cursus finibus adipiscing massa. Parturient ac proin magna consequat adipiscing adipiscing fusce.\n\nLigula sem habitasse blandit lacinia eleifend sapien libero dolor cubilia. Cras ad cubilia est at fusce vivamus. Volutpat risus tortor duis enim lacinia per aliquam. Justo eleifend id neque purus; dapibus mus vestibulum et dis. Hac dui sollicitudin; luctus vel finibus rutrum nostra. Tristique dui tristique dapibus commodo turpis dolor placerat etiam. Vestibulum cursus urna facilisis interdum fringilla. Scelerisque egestas pellentesque ipsum nulla sem sapien orci torquent mauris.\n\nMalesuada neque taciti tempus maximus ex duis. Sociosqu fringilla porta mattis mattis in class. Ridiculus dui montes tortor porta sollicitudin. Ad dui odio ultrices elit suscipit. Torquent lacus penatibus eros vel nulla pretium inceptos accumsan cursus. Ex egestas netus ridiculus auctor ligula non aptent. Maximus risus vitae fringilla rhoncus nullam varius. Hendrerit inceptos pretium dis; neque mi consequat. Eleifend maximus quisque aptent urna sagittis tortor. Ornare lacus mi lobortis faucibus, faucibus quis elit faucibus.",
+      mood: "Lorem Ipsum",
+    );
+  }
+}
+
+// Mood Entry is mostly done.
+// - Figure out how to format with paragraph breaks
+class MoodEntry extends StatelessWidget {
+  final today = DateTime.now();
+  final DateFormat date = DateFormat('dd MMMM yyy');
+  final DateFormat time = DateFormat('H:m');
+
+  final String entry;
+  final String mood;
+  MoodEntry({Key? key, required this.entry, required this.mood}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Container(
-          // entry spacing
-          margin: const EdgeInsets.all(30),
+          // entry space around the page
+          margin: const EdgeInsets.only(top: 30, bottom: 30, right: 30, left: 60),
           padding: const EdgeInsets.all(20),
           decoration: const BoxDecoration(
-            // styling
+            // background styling
             color: Color.fromARGB(255, 185, 163, 192),
             borderRadius: BorderRadius.all(Radius.circular(20)),
           ),
@@ -147,13 +169,17 @@ class MoodLoggingPage extends StatelessWidget {
             alignment: Alignment.topLeft,
             child: ListView(
               children: [
-                const Text(
-                  '20 February 2025',
+                Text(
+                  date.format(today),
                   style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30, color: Colors.black),
                   textAlign: TextAlign.left,
                 ),
                 Text(
-                  "Today I am feeling very Lorem ipsum odor amet, consectetuer adipiscing elit. Quam ullamcorper lacinia vehicula ornare lacinia interdum tincidunt cras. Est cras facilisis mauris mattis nascetur. Ligula ipsum bibendum himenaeos sed tortor nec. Cras dapibus ridiculus a nibh ridiculus interdum condimentum cursus. Interdum odio sapien vitae, mattis cursus finibus adipiscing massa. Parturient ac proin magna consequat adipiscing adipiscing fusce.\n\nLigula sem habitasse blandit lacinia eleifend sapien libero dolor cubilia. Cras ad cubilia est at fusce vivamus. Volutpat risus tortor duis enim lacinia per aliquam. Justo eleifend id neque purus; dapibus mus vestibulum et dis. Hac dui sollicitudin; luctus vel finibus rutrum nostra. Tristique dui tristique dapibus commodo turpis dolor placerat etiam. Vestibulum cursus urna facilisis interdum fringilla. Scelerisque egestas pellentesque ipsum nulla sem sapien orci torquent mauris.\n\nMalesuada neque taciti tempus maximus ex duis. Sociosqu fringilla porta mattis mattis in class. Ridiculus dui montes tortor porta sollicitudin. Ad dui odio ultrices elit suscipit. Torquent lacus penatibus eros vel nulla pretium inceptos accumsan cursus. Ex egestas netus ridiculus auctor ligula non aptent. Maximus risus vitae fringilla rhoncus nullam varius. Hendrerit inceptos pretium dis; neque mi consequat. Eleifend maximus quisque aptent urna sagittis tortor. Ornare lacus mi lobortis faucibus, faucibus quis elit faucibus.",
+                  "${time.format(today)} | Mood: $mood \n",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 27, color: Colors.black),
+                ),
+                Text(
+                  entry,
                   style: TextStyle(fontSize: 25, color: Colors.black, letterSpacing: .6),
                   textAlign: TextAlign.left,
                 ),
@@ -163,17 +189,5 @@ class MoodLoggingPage extends StatelessWidget {
         ),
       ),
     );
-  }
-}
-
-// Date and Entry are currently hard coded, this is used to modularise the code for maintainabillity, will be implemented after figuring out the general layout
-class MoodEntry extends StatelessWidget {
-  // const MoodEntry({super.key, required this.date, this.entry}) : super(key: key);
-  final DateTime date;
-  final String entry;
-  const MoodEntry({Key? key, required this.date, required this.entry}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
   }
 }
