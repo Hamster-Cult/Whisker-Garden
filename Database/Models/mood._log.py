0878acf2183147538492679047a3dc36c27fd342
Chloeluke <158167628+Chloeluke@users.noreply.github.com>
2025-02-27 11:38:31 +0000

    Update mood._log.py

--- Database/Models/mood._log.py
+++ Database/Models/mood._log.py
@@ -1,4 +1,17 @@
 from typing import Annotated
-
 from fastapi import Depends, FastAPI, HTTPException, Query
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from sqlmodel import Field, Session, SQLModel, create_engine, select, Column
+from datetime import date, time
+
+
+class mood_log(SQLModel, table=True):
+  mood_id: int | None = Field(default=None, primary_key=True)
+  mood_date: date = Field (date, nullable=False)
+  mood_time: time =  Field (nullable=False)
+  mood_rating: int = Field (nullable=False, ge=1, le=5 )
+  mood_text: str | None = Field (default=None)
+
+
+
+
+ 
